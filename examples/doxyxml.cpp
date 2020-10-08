d6490b5372468705f601845556819b674554e5a0
Gulliver <gulliver@traumkristalle.net>
2020-10-09 00:05:13 +0200

    initial working build

---
+++ examples/doxyxml.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+  std::cout << "bling" << std::endl;
+  return -1;
+}
+
---
+++ src/xmlparser.hpp
@@ -0,0 +1,33 @@
+#ifndef litesql_xmlparser_hpp
+#define litesql_xmlparser_hpp
+
+#include <string>
+// TODO: fix in expat cmake files
+#undef _MSC_EXTENSIONS
+#include "expat.h"
+
+namespace xml {
+/** abstract base class for SAX2 Parser based on libxml2 */
+class XmlParser {
+
+public:
+   virtual ~XmlParser() = default;
+   bool parseFile(const std::string& filename);
+   
+   static const XML_Char* xmlGetAttrValue(const XML_Char** attrs,const XML_Char* key);
+
+protected:
+   virtual void onStartElement(	const XML_Char *fullname,
+                                 const XML_Char **atts)	= 0;
+   virtual void onEndElement(	const XML_Char *fullname)   = 0;
+
+private:
+   friend void XMLParser_xmlSAX2StartElement		(void *ctx,
+						 const XML_Char *fullname,
+						 const XML_Char **atts);
+
+   friend void XMLParser_xmlSAX2EndElement(void *ctx,const XML_Char *name);
+};
+}
+#endif
+
