e3d447cba7f9f776e1485ca68bb50ab17c9f9288
Gulliver <gulliver@traumkristalle.net>
2021-01-19 11:52:48 +0100

    extended documentation main page introduced expatpp.hpp header put all headers in separate CMake variable in cmake script

---
+++ src/expatpp.hpp
@@ -0,0 +1,11 @@
+/**
+ * \file expatpp.hpp contains the expatpp libraries interface header
+ *
+ * See LICENSE for copyright information.
+ */
+#ifndef expatpp_hpp
+#define expatpp_hpp
+
+#include "xmlparser.hpp"
+
+#endif // #ifndef expatpp_hpp
