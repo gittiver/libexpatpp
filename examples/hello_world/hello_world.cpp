47184e8d508c191c482e5a368235b1eec596476b
Gulliver <gulliver@traumkristalle.net>
2021-01-23 17:23:26 +0100

    cleaned up hello_world.cpp

--- examples/hello_world/hello_world.cpp
+++ examples/hello_world/hello_world.cpp
@@ -4,23 +4,18 @@
  * See LICENSE for copyright information.
  */
 #include <iostream>
-#include "xmlparser.hpp"
-#include <vector>
+#include "expatpp.hpp"
 
 using std::cout;
 using std::endl;
 
-using xmlpp::parser;
-using xmlpp::delegate;
-
+/** a parser delegate which extracts the character data from an xml element*/
 class element_delegate : public xmlpp::abstract_delegate {
 public:
   std::string fullname;
   std::string characterData;
 
-  void onStartElement( const XML_Char *fullname,
-		       const XML_Char **atts) override
-  {
+  void onStartElement( const XML_Char *fullname, const XML_Char **atts) override {
     this->fullname = fullname;
   }
  
@@ -30,26 +25,26 @@ public:
 };
 
 int main(int argc,char** argv) {
-  // prepare xml input
+  // prepare xml input, 
   const char* xml = "<p>hello world!</p>";
 
   // use the delegate to extract character data from input string
-  element_delegate d;
+  element_delegate d;
 
   // parse xml input using delegate d's callbacks
-  xmlpp::parser::result res = parser::parseString(xml,d);
+  xmlpp::parser::result res = xmlpp::parser::parseString(xml,d);
 
   switch(res) {
 
   case xmlpp::parser::result::OK:
     // and output the extracted data in case of successful parsing
-    cout << d.characterData << endl;
+    cout << "element " << d.fullName << endl
+	 << " contains this text: "<< d.characterData << endl;
     return EXIT_SUCCESS;
 
   default:
     cout << "error " << static_cast<int>(res)
 	 << " on parsing " << endl;
     return -static_cast<int>(res);
-
   }
 }
